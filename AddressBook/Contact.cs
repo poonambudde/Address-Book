dd1449bb0f4a1e2e96f9c3428504e7f50582902a
poonambudde <buddepoonam123@gmail.com>
2022-02-19 15:14:44 +0530

    [Poonam] ADDED: Create contact details

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string address { get; set; }
+
+        public string city { get; set; }
+
+        public string state { get; set; }
+
+        public string zip { get; set; }
+
+        public string phoneNo { get; set; }
+
+        public string email { get; set; }
+    }
+}
