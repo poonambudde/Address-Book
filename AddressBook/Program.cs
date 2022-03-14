0bd02d3b6d9a00e820ef79f2a1f1cf0c79a52c43
poonambudde <buddepoonam123@gmail.com>
2022-03-14 13:32:22 +0530

    [Poonam] Added: Search Person in a City or State across the multiple AdressBook

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
@@ -6,9 +7,128 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Programs");
-            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Welcome to Address Book program");
 
+            Dictionary<string, AddressBook> adressBookDictionary = new Dictionary<string, AddressBook>();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("How many adress book you want = ");
+                    int numOfAdressBook = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 1; i <= numOfAdressBook; i++)
+                    {
+                        Console.WriteLine("Enter the name of adress book = " + i + "=");
+                        string adressBookName = Console.ReadLine();
+                        AddressBook adressBook = new AddressBook();
+                        adressBookDictionary.Add(adressBookName, adressBook);
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter integer number,\n string is not allowes \n enter unique name for book \n duplicate name not allowed");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("You have created following adress book");
+                    foreach (string k in adressBookDictionary.Keys)
+                    {
+                        Console.WriteLine(k);
+                    }
+                    Console.WriteLine("\n 1 for Add Contact \n 2 for Edit Existing Contact \n 3 for delete the person,\n 4 for display,\n 5 for exit");
+                    int choise = Convert.ToInt32(Console.ReadLine());
+                    switch (choise)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the Adress book name where you want to add contact");
+                            string addContactInAdressBook = Console.ReadLine();
+                            if (adressBookDictionary.ContainsKey(addContactInAdressBook))
+                            {
+                                Console.WriteLine("Enter how many contact you want to add");
+                                int numOfContact = Convert.ToInt32(Console.ReadLine());
+                                for (int i = 1; i <= numOfContact; i++)
+                                {
+                                    takeInputAndAddToContact(adressBookDictionary[addContactInAdressBook]);
+                                }
+                                adressBookDictionary[addContactInAdressBook].displayContact();
+                            }
+                            else
+                            {
+                                Console.WriteLine("No adress book found ", addContactInAdressBook);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter the Adress book name where you want to edit contact = ");
+                            string editContactInAdressBook = Console.ReadLine();
+                            if (adressBookDictionary.ContainsKey(editContactInAdressBook))
+                            {
+                                Console.WriteLine("Enter first name to edit contact =");
+                                string firstNameForEditContact = Console.ReadLine();
+                                adressBookDictionary[editContactInAdressBook].Edit(firstNameForEditContact);
+                                adressBookDictionary[editContactInAdressBook].displayContact();
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter the Adress book name where you want to delete contact = ");
+                            string deleteContactInAdressBook = Console.ReadLine();
+                            if (adressBookDictionary.ContainsKey(deleteContactInAdressBook))
+                            {
+                                Console.WriteLine("Enter first name to delete contact =");
+                                string firstNameForDeleteContact = Console.ReadLine();
+                                adressBookDictionary[deleteContactInAdressBook].delete(firstNameForDeleteContact);
+                                adressBookDictionary[deleteContactInAdressBook].displayContact();
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter the Adress book name to display contact = ");
+                            string displayContactInAdressBook = Console.ReadLine();
+                            adressBookDictionary[displayContactInAdressBook].displayContact();
+                            break;
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Enter The Valid Choise");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("please enter integer options only");
+                }
+            }
+        }
+
+        public static void findByCityOrState(Dictionary<string, AddressBook> adressBookDictionary)
+        {
+            Console.WriteLine("Enter the city or state where you want to find that person = ");
+            string findPlace = Console.ReadLine();
+            foreach (var element in adressBookDictionary)
+            {
+                List<string> listOfPersonsInPlace = element.Value.findPersons(findPlace);
+                if (listOfPersonsInPlace.Count == 0)
+                {
+                    Console.WriteLine("No person in that city/state of adress book  = " + element.Key);
+                }
+                else
+                {
+                    Console.WriteLine("The person in that city/state of adress book = " + element.Key + " = ");
+                    foreach (var names in listOfPersonsInPlace)
+                    {
+                        Console.WriteLine(names);
+                    }
+                }
+            }
+        }
+
+        public static void takeInputAndAddToContact(AddressBook adressBook)
+        {
             Console.WriteLine("Enter firstName");
             string firstName = Console.ReadLine();
 
@@ -33,15 +153,14 @@ namespace AddressBook
             Console.WriteLine("Enter state");
             string state = Console.ReadLine();
 
-            addressBook.addContact(firstName, lastName, email, phoneNumber, address, zip, city, state);
-
-            Console.WriteLine("\nIf you want to edit contact plz enter name");
-            string name = Console.ReadLine();
-            addressBook.Edit(name);
-
-            Console.WriteLine("\nIf you want to delete contact plz enter name");
-            string firstname = Console.ReadLine();
-            addressBook.Remove(firstname);
+            if ((firstName != "") || (lastName != "") || (address != "") || (city != "") || (state != "") || (zip != "") || (email != "") || (phoneNumber != ""))
+            {
+                adressBook.addContact(firstName, lastName, email, phoneNumber, address, zip, city, state);
+            }
+            else
+            {
+                Console.WriteLine("Empty string not allowed \n for add contacts please give the input in string");
+            }
         }
     }
 }
